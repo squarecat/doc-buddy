8144b22133202b84c209b71d42862d0727400dac
James Ivings <james@squarecat.io>
2023-04-30 14:28:10 +0200

    add status route

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import Koa from "koa";
 import dotenv from "dotenv";
+import router from "@koa/router";
 
 dotenv.config({});
 
 const { launch } = await import("./src/bot.js");
 launch();
-console.log("Bot listening...");
+console.log("[server]: Bot listening...");
 
 const app = new Koa();
-app.listen(process.env.PORT);
+
+const statusRouter = router().get("/", (ctx) => {
+  ctx.body = {
+    _version: 0.1,
+  };
+});
+
+app.use(statusRouter.routes());
+
+app.listen(process.env.PORT || 1333);
+console.log(`[server]: started on ${process.env.PORT || 1333}`);
